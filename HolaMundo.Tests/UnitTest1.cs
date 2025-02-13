0011235d2caef5ae3570c17f788a0067fa83c153
Kimberly Jazlin Jiménez <jjimjazlk@gmail.com>
2025-02-13 02:45:45 -0400

    HolaMundo + UnitTest

---
+++ HolaMundo.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using HolaMundo;
+
+namespace HolaMundo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestSaludar()
+        {
+            string resultado = Program.Saludar();
+            Assert.Equal("Hola Mundo!", resultado);
+        }
+    }
+}
---
+++ HolaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    public class Program
+    {
+        public static string Saludar()
+        {
+            return "Hola Mundo!";
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Saludar());
+        }
+    }
+}
